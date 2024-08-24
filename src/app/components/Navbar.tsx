aec2a989bb3799c8e5c94187e79ff060c24684dd
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2024-08-24 21:52:40 +0700

    Revert "Add props to card"

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,7 @@
 export default function Navbar(){
     return(
-        <nav>
-            <img 
-                src="" 
-                alt=""
-                />
-            <h1>STARTDEE</h1>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-            </ul>
-        </nav>
+        <div>
+            This is nav components
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
-import info from "./info"
 
 export default function Home() {
-  
   return (
     <main>
       <Navbar />
-      {
-        info.map((data) => (
-          <Card 
-            key={data.id}
-            {...data}
-          />
-        ))
-      }
+      <Card />
     </main>
   );
 }
