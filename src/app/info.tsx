644fc17a9fbd08f9d39c10c205ee6e3ba73fa0a5
Aomsub101 <lerdariyamaythee@gmail.com>
2024-08-24 21:47:23 +0700

    Add props to card
    Add some of the data to info files and display.

---
+++ src/app/info.tsx
@@ -0,0 +1,45 @@
+interface User {
+    id:number
+    name:string
+    role:string
+}
+
+const users: User[] = [
+    {
+        id:1,
+        name: "Aomsub",
+        role:"Front-End Developer"
+    },
+    {
+        id:2,
+        name: "Amp",
+        role:"Designer"
+    },
+    {
+        id:3,
+        name: "Drake",
+        role:"Mangement"
+    },
+    {
+        id:4,
+        name: "Gun",
+        role:"Designer"
+    },
+    {
+        id:5,
+        name: "Palm",
+        role:"Front-End Developer"
+    },
+    {
+        id:6,
+        name: "Eng",
+        role:"Designer"
+    },
+    {
+        id:7,
+        name: "Champ",
+        role:"Designer"
+    }
+]
+
+export default users
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import info from "./info"
 
 export default function Home() {
+  
   return (
     <main>
       <Navbar />
-      <Card />
+      {
+        info.map((data) => (
+          <Card 
+            key={data.id}
+            {...data}
+          />
+        ))
+      }
     </main>
   );
 }
