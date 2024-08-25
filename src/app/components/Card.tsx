67d39c86390d70bd0db2bf47cce236f529878db0
Aomsub101 <lerdariyamaythee@gmail.com>
2024-08-25 14:00:19 +0700

    Fix Card & change file format

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -15,28 +15,19 @@ export default function Card({ image, firstName, lastName, role, linkedIn, email
           alt={`${firstName}'s picture.`}
           className="border border-black border-2 w-36 h-36 object-cover rounded-full float-start mr-6"
         />
-      <div className="border border-black h-36 w-385 rounded-full">  
+      <div className="border border-black h-36 w-385 rounded-full py-2.5">  
         <div>
           <h1 className="text-xl font-bold">{firstName}</h1>
           <h1 className="text-xl font-bold">{lastName}</h1>
           <h4 className="font-bold">{role}</h4>
-          <div className="flex flex-row">
-            <img 
-              src="./image/gmail.png" 
-              alt="gmail icon."
-              className="w-6 mr-4"
-            />
-            <p>{email}</p>
-          </div>
-          <a href={linkedIn} target="_blank">
-            <img 
-              src="./image/linkedin.png" 
-              alt="linkedin icon." 
-              className="w-6"
-            />
-          </a>
-          
         </div>
+        <a href={linkedIn} target="_blank">
+          <img 
+            src="./image/linkedin.webp" 
+            alt="linkedin icon." 
+            className="w-6 mt-3.5"
+          />
+        </a>
       </div>
     </div>
   )
